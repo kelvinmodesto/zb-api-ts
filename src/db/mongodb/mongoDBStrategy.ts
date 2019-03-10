9a28bfe0833b0ee1ff45fa17b7fd488612ebc5cd
Kelvin Modesto <km.modesto@gmail.com>
2019-03-10 01:56:48 -0300

    Change Database classes

--- src/db/mongodb/mongoDBStrategy.ts
+++ src/db/mongodb/mongoDBStrategy.ts
@@ -2,10 +2,37 @@ import mongoose from 'mongoose';
 
 import { Context } from '../base/contextStrategy';
 
-export default class MongoDBStrategy extends Context {
+export default class MongoStrategy extends Context {
   public connection: any;
+  public model: any;
 
-  constructor(connection, model) {
+  constructor(connection: any, model: any) {
+    super(connection);
+    this.connection = connection;
+    this.model = model;
+  }
+
+  public async create(item: any) {
+
+  }
+
+  public async read(item: any) {
+
+  }
+
+  public async update(item: any) {
+
+  }
+
+  public async delete(id: number) {
+
+  }
+
+  public async isConnected() {
+
+  }
+
+  public async connect() {
 
   }
 }
