5a1713f5b97d34940dacf4baca29764eda806150
Kelvin Modesto <km.modesto@gmail.com>
2019-03-10 12:52:24 -0300

    Create userRouter

---
+++ src/api/userRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+// Database
+import * as Context from '../db/base/contextStrategy';
+import * as MongoDB from '../db/mongodb/mongoDBStrategy';
+
+// Controller
+import * as UserController from '../controllers/userController';
+
+// Model list
