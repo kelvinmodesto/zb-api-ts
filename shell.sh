00141efc7ff026649703f42d1fa7a498780090b2
Kelvin Modesto <km.modesto@gmail.com>
2019-03-12 23:49:44 -0300

    Change shell file

--- shell.sh
+++ shell.sh
@@ -6,12 +6,3 @@ docker pull mysql
 
 docker run  --name mongodb -p 27017:27017  -e MONGO_INITDB_ROOT_USERNAME="admin"  -e MONGO_INITDB_ROOT_PASSWORD="admin"  -d
  mongo
-
-docker run --name mongoclient -p 5000:3000 --link mongodb:mongodb -d mongoclient/mongoclient
-
-docker exec -it mongodb mongo --host localhost -u "admin" -p "admin" --authenticationDatabase admin --eval
-"db.getSiblingDB('tools').createUser({user:'kelvinmodesto', pwd: 'admin', roles: [{role: 'readWrite', db: 'tools'}]})"
-
-docker exec -it mongodb mongo --host localhost -u "admin" -p "admin" --authenticationDatabase admin --eval
-"db.tools"
-
