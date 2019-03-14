c151667c473339f25152923f747a4fcd86681401
Kelvin Modesto <km.modesto@gmail.com>
2019-03-13 23:55:03 -0300

    Create connection string postgres method

--- src/db/postgres/postgresStrategy.ts
+++ src/db/postgres/postgresStrategy.ts
@@ -3,6 +3,12 @@ import sequelize from 'sequelize';
 import { ContextStrategy as Context } from '../base/contextStrategy';
 
 export class PostgresStrategy extends Context {
+  public static buildConnectionString() {
+    return `${process.env.DB_POSTGRES}://${process.env.DB_USER_POSTGRES}:
+      ${process.env.DB_PASS_POSTGRES}@${process.env.DB_HOST_POSTGRES}:
+      ${process.env.DB_PORT_POSTGRES}/${process.env.DB_NAME_POSTGRES}`;
+  }
+
   public static connect() {
 
   }
